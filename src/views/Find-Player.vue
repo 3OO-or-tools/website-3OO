e551750db544e51225b4f3cb97faaf6dfce258be
Maxime VINCENT <maxime2.vincent@epitech.eu>
2021-03-13 22:34:46 +0100

    Change a bit the view

--- src/views/Find-Player.vue
+++ src/views/Find-Player.vue
@@ -1,25 +1,33 @@
 <template>
-  <div class="find-player">
-      <input type="text" v-model="player.name" @change="findUser">
-      <table class="table">
-        <thead>
-          <tr>
-            <th>Galaxie</th>
-            <th>Système</th>
-            <th>Position</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(planet, i) in player.planets" :key="`planet-${i}`">
-            <td>{{ planet.g }}</td>
-            <td>{{ planet.s }}</td>
-            <td>{{ planet.p }}</td>
-            <td>{{ planet.date_updated }}</td>
-          </tr>
-        </tbody>
-      </table>
-  </div>
+    <div class="container find-player">
+        <div class="row">
+            <div class="input-group">
+                <span class="input-group-text">Pseudo</span>
+                <input type="text" class="form-control" v-model="player.name" @change="findUser">
+                <button class="btn btn-outline-secondary" type="button" @click="findUser">Trouver</button>
+            </div>
+        </div>
+        <div class="row">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Galaxie</th>
+                        <th>Système</th>
+                        <th>Position</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(planet, i) in player.planets" :key="`planet-${i}`">
+                        <td>{{ planet.g }}</td>
+                        <td>{{ planet.s }}</td>
+                        <td>{{ planet.p }}</td>
+                        <td>{{ (new Date(planet.date_updated)).toLocaleString() }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </template>
 
 <script>
